6df4a9b5447d01eac3761502a7adafd19b86d193
Daniel Vals <145558974+DaniVals@users.noreply.github.com>
2023-10-23 13:43:23 +0200

    PT

---
+++ Programacion/Java/EjerciciosJava/src/EjerciciosObjetos/O02pokemonLand/MainPokemon.java
@@ -0,0 +1,17 @@
+package EjerciciosObjetos.O02pokemonLand;
+
+public class MainPokemon {
+    
+    public static void main(String[] args) {
+        Pokemon pokemon1, pokemon2;
+
+        pokemon1= new Pokemon('W',7,3,100);
+        pokemon1.mostrarStats();
+        pokemon2= new Pokemon('F',6,4,10);
+        System.out.println(pokemon2);
+
+        //pokemon2.comerBaya();
+
+        System.out.println("El ganador es el pokemon "+pokemon1.lucha(pokemon2));
+    }
+}
