e8b28074d3b8597f1ef5db9c5cf6c68beefff36f
Daniel Vals <145558974+DaniVals@users.noreply.github.com>
2023-11-02 13:38:49 +0100

    PT prueba gab

--- Programacion/Java/EjerciciosJava/src/FuncionesCopias/MenuWhileSwitch.java
+++ Programacion/Java/EjerciciosJava/src/FuncionesCopias/MenuWhileSwitch.java
@@ -5,18 +5,26 @@ import java.util.Scanner;
 public class MenuWhileSwitch {
     public static void main(String[] args){
         
+        Mates mates;
+        mates=new Mates();
+
+        System.out.println(mates.factorial(5));
+        
+        
+
+
+
         Scanner teclado=new Scanner(System.in);
         int opcion = 1;
-
         while (opcion!=0){
             System.out.println("Elije una opcion: \n 1. 2. 3.    0.salir");
             opcion= teclado.nextInt();
             switch (opcion){
-                case 1://ingresar
+                case 1:
                 break;
-                case 2://retirar
+                case 2:
                 break;
-                case 3://mostrar datos
+                case 3:
                 break;
             }
         }
