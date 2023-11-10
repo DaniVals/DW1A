53e0e5d4772ba02ac4a5b24d8a0b36dde14dda42
Daniel Vals <145558974+DaniVals@users.noreply.github.com>
2023-11-10 10:41:40 +0100

    PT

---
+++ Bases de datos/scripts SQL/ejemplos CONSTRAIN.sql
@@ -0,0 +1,19 @@
+CREATE TABLE pais(
+    id NUMBER CONSTRAINT usuario_id_PK PRIMARY KEY,
+    nombre VARCHAR(32)
+    );
+INSERT INTO pais VALUES(1, 'Japon');
+
+CREATE TABLE usuario (
+    --id NUMBER(6) CONSTRAINT usuario_id_PK PRIMARY KEY,
+    nombre VARCHAR2(20),
+    --contrasena VARCHAR2(8)	CONSTRAINT usuario_contrasena_NN NOT NULL --NO HAY COMA
+    --						CONSTRAINT usuario_contrasena_UQ UNIQUE,
+    localizacion NUMBER CONSTRAINT usuario_localizacion_FK REFERENCES pais(id)
+    
+	);
+SELECT * FROM pais;
+SELECT * FROM usuario;
+DESC usuario;
+DROP TABLE usuario;
+DROP TABLE pais;
