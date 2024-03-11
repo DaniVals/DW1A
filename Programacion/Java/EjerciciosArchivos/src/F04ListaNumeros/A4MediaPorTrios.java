19ddceba67e5fec343799d8b9753a5cb4986e96a
Daniel Vals <daniel.vals.simon@gmail.com>
2024-03-11 13:40:55 +0100

    PT

--- Programacion/Java/EjerciciosArchivos/src/F04ListaNumeros/A4MediaPorTrios.java
+++ Programacion/Java/EjerciciosArchivos/src/F04ListaNumeros/A4MediaPorTrios.java
@@ -5,14 +5,6 @@ A partir del fichero notas.txt, leer los números de tres en tres (suponer que h
 cantidad de valores múltiplo de 3) y crear un nuevo fichero que contendrá en cada línea 
 la media de esos tres valores.
 
-
-
- 
-
- 
-7. 
- 
-8. 
 */
 public class A4MediaPorTrios {
 
--- Programacion/Java/EjerciciosArchivos/src/F05Palabras/Main.java
+++ Programacion/Java/EjerciciosArchivos/src/F05Palabras/Main.java
@@ -1,11 +1,58 @@
 package src.F05Palabras;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /*
 Crear un fichero de texto que guarde una palabra en cada línea. Mostrar por pantalla 
 la  palabra  con  más  caracteres  (en  caso  de  empate  se  puede  mostrar  la  que  el  alumno 
 desee). También crear un fichero con las palabras que son palíndromos (una por línea). 
 */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            in = new BufferedReader(new FileReader("Programacion/Java/EjerciciosArchivos/src/F05Palabras/palabras.txt"));
+            out = new PrintWriter(new FileWriter("Programacion/Java/EjerciciosArchivos/src/F05Palabras/palindromos.txt"));
+            
+            String l, larga = "";
+            while ((l = in.readLine()) != null) {
+                if (larga.length() < l.length()) {
+                    larga = l;
+                }
+                if (esPalindromo(l)) {
+                    out.println(l);
+                }
+            }
+
+            System.out.println("larga: "+larga);
+
+        }
+        finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+    public static boolean esPalindromo(String s){
+        
+        String textoAsist="";
+
+        for (int i=s.length()-1; i>=0;i--){
+            textoAsist+=s.charAt(i);
+        }
+        
+        if (s.compareTo(textoAsist)==0) {
+            return true;
+        }
+        return false;
     }
 }
