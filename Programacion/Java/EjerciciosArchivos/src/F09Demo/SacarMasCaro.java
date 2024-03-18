6f29f334394f08e5548d1903965efddd9ee51edb
Daniel Vals <daniel.vals.simon@gmail.com>
2024-03-18 12:47:23 +0100

    PT

--- Programacion/Java/EjerciciosArchivos/src/F09Demo/SacarMasCaro.java
+++ Programacion/Java/EjerciciosArchivos/src/F09Demo/SacarMasCaro.java
@@ -1,12 +1,9 @@
 package src.F09Demo;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 /* 
